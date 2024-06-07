ed168823a447b54d0cfb9fd84b293e4f1b688ab8
Joe Harrison <{ID}+{username}@users.noreply.github.com>
2024-06-07 09:24:20 -0700

    P8 image resize example

--- 8 moonshot/8 moonshot/ContentView.swift
+++ 8 moonshot/8 moonshot/ContentView.swift
@@ -7,12 +7,69 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SpecialTitle: View {
+    var titleText: String
     var body: some View {
-        VStack {
+        ZStack {
+        
+        Text("Bible Study")
+            .font(.largeTitle)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .fontDesign(.serif)
+            .foregroundStyle(.green)
+            .padding(20)
+            .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
             
+        Text("Bible Study")
+            .font(.largeTitle)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .fontDesign(.serif)
+            .foregroundStyle(.black)
+            .padding()
+    }
+    }
+}
+
+struct TopicImage: View {
+    var pic: ImageResource
+    var body: some View {
+        ZStack{
+            
+            Image(.study)
+                .resizable()
+                .frame( width: 120, height: 80)
+                .clipShape(.capsule)
+                .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+            Image(.study)
+                .resizable()
+                .frame( width: 110, height: 70)
+                .clipShape(.capsule)
+                .padding()
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    var pics: [ImageResource] = [.bible, .latin, .manuscript, .paintedBible, .study]
+    var body: some View {
+        ZStack 
+        {
+            //BACKGROUND
+            Image(.bible)
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            VStack
+            {
+                SpecialTitle(titleText: "Bible Study")
+                ScrollView
+                {
+                    ForEach (0..<5) { index in
+                        TopicImage(pic: pics[index])
+                    }.padding()
+                }
+            }
+        }
     }
 }
 
