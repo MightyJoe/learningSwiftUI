150ebadaf9b87ee0b7f9f1e2842e09c572abe499
Joe Harrison <{ID}+{username}@users.noreply.github.com>
2024-06-11 12:47:49 -0700

    json to object start...

--- 8 moonshot/8 moonshot/ContentView.swift
+++ 8 moonshot/8 moonshot/ContentView.swift
@@ -6,62 +6,72 @@
 //
 
 import SwiftUI
+//
+//struct SpecialTitle: View {
+//    var titleText: String
+//    var body: some View {
+//        ZStack {
+//        
+//        Text("Bible Study")
+//            .font(.largeTitle)
+//            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+//            .fontDesign(.serif)
+//            .foregroundStyle(.green)
+//            .padding(20)
+//            .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+//            
+//        Text("Bible Study")
+//            .font(.largeTitle)
+//            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+//            .fontDesign(.serif)
+//            .foregroundStyle(.black)
+//            .padding()
+//    }
+//    }
+//}
+//
+//struct TopicImage: View {
+//    var pic: String
+//    var body: some View {
+//        ZStack{
+//            
+//            Image(pic)
+//                .resizable()
+//                .frame( width: 120, height: 80)
+//                .clipShape(.capsule)
+//                .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+//            Image(pic)
+//                .resizable()
+//                .frame( width: 110, height: 70)
+//                .clipShape(.capsule)
+//                .padding()
+//        }
+//    }
+//}
+//
+//struct CustomText: View {
+//    let text: String
+//
+//    var body: some View
+//    {
+//        Text(text)
+//    }
+//    
+//    init(_ passedIn: String)
+//    {
+//        print("Creating a new custom text")
+//        self.text = passedIn
+//    }
+//}
 
-struct SpecialTitle: View {
-    var titleText: String
-    var body: some View {
-        ZStack {
-        
-        Text("Bible Study")
-            .font(.largeTitle)
-            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            .fontDesign(.serif)
-            .foregroundStyle(.green)
-            .padding(20)
-            .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-            
-        Text("Bible Study")
-            .font(.largeTitle)
-            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            .fontDesign(.serif)
-            .foregroundStyle(.black)
-            .padding()
-    }
-    }
+struct User: Codable {
+    let name: String
+    let address: Address
 }
 
-struct TopicImage: View {
-    var pic: String
-    var body: some View {
-        ZStack{
-            
-            Image(pic)
-                .resizable()
-                .frame( width: 120, height: 80)
-                .clipShape(.capsule)
-                .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-            Image(pic)
-                .resizable()
-                .frame( width: 110, height: 70)
-                .clipShape(.capsule)
-                .padding()
-        }
-    }
-}
-
-struct CustomText: View {
-    let text: String
-
-    var body: some View
-    {
-        Text(text)
-    }
-    
-    init(_ passedIn: String)
-    {
-        print("Creating a new custom text")
-        self.text = passedIn
-    }
+struct Address: Codable {
+    let street: String
+    let city: String
 }
 
 struct ContentView: View {
@@ -72,6 +82,18 @@ struct ContentView: View {
                 NavigationLink("Row \(row)"){
                     Text("Detail \(row)")
                 }
+            }
+            Button("JSON"){
+                let jsonTestInput = """
+{
+    "name": "John Candy",
+    "address": 
+    {
+        "street": "1234Candy Lane",
+        "city": "Ontario"
+    }
+}
+"""
             }
         }.navigationTitle("RowExample")
     }
