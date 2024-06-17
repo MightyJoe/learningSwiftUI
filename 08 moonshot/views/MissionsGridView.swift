8d869e2b9afbff0cb83147926c7076f6c19482a1
Joe Harrison <{ID}+{username}@users.noreply.github.com>
2024-06-17 15:55:22 -0700

    separated gridview from contentView

---
+++ 08 moonshot/views/MissionsGridView.swift
@@ -0,0 +1,67 @@
+//
+//  MissionsGridView.swift
+//  8 moonshot
+//
+//  Created by Joe Harrison on 2024-06-17.
+//
+
+import SwiftUI
+
+struct MissionsGridView: View {
+    
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    var body: some View {
+        
+                LazyVGrid(columns: columns)
+                {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label: {
+                            VStack
+                            {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.7))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }.padding([.horizontal, .bottom])
+            
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ScrollView {
+            MissionsGridView()
+                .preferredColorScheme(.dark)
+        }.navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+    }
+}
