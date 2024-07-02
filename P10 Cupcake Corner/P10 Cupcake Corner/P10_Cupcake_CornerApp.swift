30569bee681cde950e9426d249cabc5d572dd256
Joe Harrison <{ID}+{username}@users.noreply.github.com>
2024-07-02 12:37:01 -0700

    Start of P10

---
+++ P10 Cupcake Corner/P10 Cupcake Corner/P10_Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  P10_Cupcake_CornerApp.swift
+//  P10 Cupcake Corner
+//
+//  Created by Joe Harrison on 2024-07-02.
+//
+
+import SwiftUI
+
+@main
+struct P10_Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
