b5a3ff7107cf3b3a22023c8d3d7bb3817a194c5a
Joe Harrison <4879956+MightyJoe@users.noreply.github.com>
2024-07-17 13:43:43 -0700

    P10 CheckoutView added

---
+++ P10 Cupcake Corner/CheckoutView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutView.swift
+//  P10 Cupcake Corner
+//
+//  Created by Joe Harrison on 2024-07-17.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
