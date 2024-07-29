e9f3670a9432d93e8bbe499f2834de7f7b6687ea
Joe Harrison <4879956+MightyJoe@users.noreply.github.com>
2024-07-28 20:29:16 -0700

    restructured P11

---
+++ 11 bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Joe Harrison on 2024-07-25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
